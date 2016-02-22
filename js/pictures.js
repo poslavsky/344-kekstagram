2914b2a6c8a2d116949f6db9dd2a45b0672cf9cc
poslavsky <mishapsv@gmail.com>
2016-02-22 23:10:23 +0300

    task done

--- js/pictures.js
+++ js/pictures.js
@@ -1,83 +1,75 @@
-/* global pictures: true */
-
 'use strict';
 
 (function() {
-    var container = document.querySelector('.pictures');
-    var template = document.querySelector('#picture-template');
-    var filters = document.querySelector('.filters');
-    var filtersElements = document.querySelectorAll('.filters-item');
-    var pictures = [];
-//    var activeFilter = 'filter-popular';
-    filters.classList.add('hidden');
+  var container = document.querySelector('.pictures');
+  var template = document.querySelector('#picture-template');
+  var filters = document.querySelector('.filters');
+  var filtersButton = document.querySelectorAll('.filters-item');
+  var pictures = [];
+  filters.classList.add('hidden');
 
-    for (var i = 0; i < filtersElements.length; i++) {
-      filtersElements[i].onclick = function(e) {
-        var clickedElementID = e.target.htmlFor;
-        console.log(clickedElementID);
-        setActiveFilter(clickedElementID);
-      };
-    }
-
-    function setActiveFilter(id) {
-      // if (activeFilter === id) {
-      //   return;
-      // }
-      var sortPictures = pictures.slice(0);
-      switch (id) {
-        case "filter-new":
-          sortPictures = sortPictures.sort(function(a, b) {
-            return Date.parse(a.date) - Date.parse(b.date);
-          });
-          break;
-        case "filter-discussed":
-          sortPictures = sortPictures.sort(function(a, b) {
-            return a.comments - b.comments;
-          });
-          break;
-        default:
-          sortPictures;
-          break;
-      }
-      renderPhoto(sortPictures);
-    }
-
-  getPhoto();
-
-  function renderPhoto(pictures) {
-    container.innerHTML = '';
-    pictures.forEach(function(picture) {
-      var element = getElementFromTemplate(picture);
-      container.appendChild(element);
-    });
+  for (var i = 0; i < filtersButton.length; i++) {
+    filtersButton[i].onclick = function(e) {
+      var clickedElementFor = e.target.htmlFor;
+      setActiveFilter(clickedElementFor);
+    };
   }
 
-  function getPhoto() {
+  function setActiveFilter(forID) {
+    var filteredPictures = pictures.slice(0);
+    switch (forID) {
+      case 'filter-new':
+        var twoWeeks = new Date() - 2 * 14 * 24 * 60 * 60 * 100;
+        filteredPictures = filteredPictures.filter(function(a) {
+          return Date.parse(a.date) >= twoWeeks;
+        }).sort(function(a, b) {
+          return Date.parse(b.date) - Date.parse(a.date);
+        });
+        break;
+      case 'filter-discussed':
+        filteredPictures = filteredPictures.sort(function(a, b) {
+          return b.comments - a.comments;
+        });
+        break;
+    }
+    renderPictures(filteredPictures);
+  }
+
+  getPictures();
+
+  function renderPictures(picturesArray) {
+    container.innerHTML = '';
+    var fragment = document.createDocumentFragment();
+    picturesArray.forEach(function(picture) {
+      var element = getElementFromTemplate(picture);
+      fragment.appendChild(element);
+    });
+    container.appendChild(fragment);
+  }
+
+  function getPictures() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '//o0.github.io/assets/json/pictures.json');
     xhr.onloadstart = function() {
       container.classList.add('pictures-loading');
     };
-
-    xhr.onload = function(evt) {
+    xhr.onload = function(e) {
       container.classList.remove('pictures-loading');
-      var rawData = evt.target.response;
+      var rawData = e.target.response;
+      var loadedPictures = JSON.parse(rawData);
+      pictures = loadedPictures;
+      renderPictures(loadedPictures);
+    };
 
-      var loadedPhoto = JSON.parse(rawData);
-      renderPhoto(loadedPhoto);
-      //setActiveFilter(loadedPhoto);
-    }
     xhr.send();
-
     xhr.onerror = function() {
       container.classList.add('pictures-failure');
     };
     xhr.timeout = 10000;
     xhr.ontimeout = function() {
       container.classList.add('pictures-failure');
-    }
+    };
   }
-
 
   function getElementFromTemplate(data) {
     var element;
@@ -89,10 +81,8 @@
     element.querySelector('.picture-comments').textContent = data.comments;
     element.querySelector('.picture-likes').textContent = data.likes;
 
-
     var backgroundImage = new Image(182, 182);
     var templateImg = element.querySelector('img');
-
     element.replaceChild(backgroundImage, templateImg);
     backgroundImage.src = data.url;
 
